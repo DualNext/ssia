efa480ddfc5eb4fe50a7f822970fd845bdc352f3
FotoDiagX <thiagofranssa@gmail.com>
2025-09-26 16:34:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if uploaded_file is not None:
             plt.ylabel('Transmitância Normalizada', labelpad=15, fontsize=28, fontname='Cambria')
             plt.xticks(np.arange(900, 1800 + 100, 100), fontsize=18, fontname='Cambria')
             plt.gca().tick_params(axis='x', pad=20)  # Ajusta o espaço entre os rótulos e a linha
-            plt.xlim(1800, 900)
+            plt.xlim(4000, 400)
             # plt.ylim(dados_coletados.min().min() - 0.5, 100.5)
             plt.yticks(fontsize=18, fontname='Cambria')
             plt.gca().tick_params(axis='y', pad=20)  # Ajusta o espaço entre os rótulos e a linha
@@ -124,6 +124,7 @@ else:
     st.markdown('''<h1 style="color: orange; font-size: 35px;">Diagnóstico de Brucelose Bovina</h1>''', unsafe_allow_html=True)
     # Subtítulo (h3)
     st.markdown('''<h3 style="color: white; font-size: 20px;">Carregue um espectro FTIR para análise</h3>''', unsafe_allow_html=True)
+
 
 
 
